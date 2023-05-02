34ffab04db75d7d406ba222c7d8dc55b991bcc1b
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-02 21:24:49 +0600

    navbar added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -2,8 +2,31 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header</h2>
+        <div className="navbar bg-base-100 w-[90%] mx-auto">
+            <div className="navbar-start">
+                {/* <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 3</a></li>
+                    </ul>
+                </div> */}
+                <a className="btn btn-ghost normal-case text-xl">Chinese Lounge</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Home</a></li>
+                    <li><a>Blog</a></li>
+                    <li><a>Shop</a></li>
+                    <li><a>Contact Us</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Login</a>
+                <img className='w-[40px]' src="https://cdn-icons-png.flaticon.com/512/164/164600.png?w=740&t=st=1683040299~exp=1683040899~hmac=3e533d18def3d14c1a6ea92cbeb11f5e3672a892eecb494732ebb44d669f5c53" alt="" />
+            </div>
         </div>
     );
 };
