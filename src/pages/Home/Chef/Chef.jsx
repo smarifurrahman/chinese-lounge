f05558f9fd68484d4e2e0a1f09efe5cbb30887b0
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-03 05:58:37 +0600

    chef card and  data loaded dynamically

--- src/pages/Home/Chef/Chef.jsx
+++ src/pages/Home/Chef/Chef.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { HiArrowRight } from "react-icons/hi";
 import { Link } from 'react-router-dom';
 
-const Chef = () => {
+const Chef = ({ chef }) => {
+    const { id, chef_name, years_experience, num_recipes, likes } = chef;
+
     return (
         <div className="card card-side bg-base-100 shadow-2xl">
             <figure className='w-[40%]'><img src="https://img.freepik.com/free-photo/chef-making-ok-sign-white-background_1368-2804.jpg?w=740&t=st=1683055811~exp=1683056411~hmac=acbfd5bc2f26d0a727d6dc8704c747bc10cabb795d149d3725f61d3272c402a7" alt="Movie" /></figure>
             <div className="card-body">
-                <h2 className="card-title font-playfair text-2xl font-bold hover:text-green-start mb-2">Arifur Rahman</h2>
-                <p>5 Years of experience</p>
-                <p className='my-1'>10 recipes</p>
-                <p>1.5k Likes</p>
+                <h2 className="card-title font-playfair text-2xl font-bold hover:text-green-start mb-2">{chef_name}</h2>
+                <p>{years_experience} years of experience</p>
+                <p className='my-1'>{num_recipes} recipes</p>
+                <p>{likes} Likes</p>
                 <div className="card-actions mt-2">
-                    <Link to={`/recipes/1`}>
+                    <Link to={`/recipes/${id}`}>
                         <button className="btn bg-green-start hover:bg-green-end border-green-start hover:border-green-end px-6">
                             View Recipes
                             <span className='ml-2'>
--- src/pages/Home/ChefSection/ChefSection.jsx
+++ src/pages/Home/ChefSection/ChefSection.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Chef from '../Chef/Chef';
+import { data } from 'autoprefixer';
 
 const ChefSection = () => {
+    const [chefs, setChefs] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/chefs')
+            .then(res => res.json())
+            .then(data => setChefs(data))
+            .catch(error => console.error(error))
+    })
+
     return (
         <div className='mt-20 w-[85%] mx-auto'>
             <div className='text-center mb-8'>
                 <h2 className='text-dark hover:text-green-start text-4xl font-bold mb-3 font-playfair'>Our Chefs</h2>
                 <p className='text-gray font-medium'>Explore our chefs for the best food recipes</p>
             </div>
-            <div className='grid grid-cols-2'>
-                <Chef></Chef>
+            <div className='grid grid-cols-2 gap-6'>
+                {
+                    chefs.map(chef => <Chef
+                        key={chef.id}
+                        chef={chef}
+                    ></Chef>)
+                }
             </div>
         </div>
     );
