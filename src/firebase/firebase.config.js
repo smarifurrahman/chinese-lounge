192d1df6cd5689ae80a49ca726d54fce2d666381
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-03 23:09:40 +0600

    env variable implemented

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAuJIpDped-g17Mnc6wNR1Qh1uqje35-pc",
-    authDomain: "chinese-lounge.firebaseapp.com",
-    projectId: "chinese-lounge",
-    storageBucket: "chinese-lounge.appspot.com",
-    messagingSenderId: "926381530522",
-    appId: "1:926381530522:web:d34ba9667781996ae0ae23"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
