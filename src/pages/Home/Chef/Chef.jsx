f9763f116440f15059da2dd16596b7bad8fc92cc
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-03 06:07:21 +0600

    chef card fixed

--- src/pages/Home/Chef/Chef.jsx
+++ src/pages/Home/Chef/Chef.jsx
@@ -3,25 +3,27 @@ import { HiArrowRight } from "react-icons/hi";
 import { Link } from 'react-router-dom';
 
 const Chef = ({ chef }) => {
-    const { id, chef_name, years_experience, num_recipes, likes } = chef;
+    const { id, chef_name, chef_picture, years_of_experience, num_recipes, likes } = chef;
 
     return (
         <div className="card card-side bg-base-100 shadow-2xl">
-            <figure className='w-[40%]'><img src="https://img.freepik.com/free-photo/chef-making-ok-sign-white-background_1368-2804.jpg?w=740&t=st=1683055811~exp=1683056411~hmac=acbfd5bc2f26d0a727d6dc8704c747bc10cabb795d149d3725f61d3272c402a7" alt="Movie" /></figure>
+            <figure className='w-[40%] h-[300px]'><img className='object-cover' src={chef_picture} alt="Movie" /></figure>
             <div className="card-body">
-                <h2 className="card-title font-playfair text-2xl font-bold hover:text-green-start mb-2">{chef_name}</h2>
-                <p>{years_experience} years of experience</p>
-                <p className='my-1'>{num_recipes} recipes</p>
-                <p>{likes} Likes</p>
-                <div className="card-actions mt-2">
-                    <Link to={`/recipes/${id}`}>
-                        <button className="btn bg-green-start hover:bg-green-end border-green-start hover:border-green-end px-6">
-                            View Recipes
-                            <span className='ml-2'>
-                                <HiArrowRight />
-                            </span>
-                        </button>
-                    </Link>
+                <div>
+                    <h2 className="card-title font-playfair text-2xl font-bold hover:text-green-start mb-2">{chef_name}</h2>
+                    <p>{years_of_experience} years of experience</p>
+                    <p className='my-1'>{num_recipes} recipes</p>
+                    <p>{likes} Likes</p>
+                    <div className="card-actions mt-2">
+                        <Link to={`/recipes/${id}`}>
+                            <button className="btn bg-green-start hover:bg-green-end border-green-start hover:border-green-end px-4">
+                                View Recipes
+                                <span className='ml-2'>
+                                    <HiArrowRight />
+                                </span>
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
