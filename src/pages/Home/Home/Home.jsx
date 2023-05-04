2514896b9b6936869238029145044330a3d14d3c
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-04 07:49:04 +0600

    popular recipe section added to home

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import ChefSection from '../ChefSection/ChefSection';
+import Popular from '../Popular/Popular';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <ChefSection></ChefSection>
+            <Popular></Popular>
         </div>
     );
 };
---
+++ src/pages/Home/Popular/Popular.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import Recipe from '../../ChefRecipes/Recipe/Recipe';
+import { ToastContainer } from 'react-toastify';
+
+const Popular = () => {
+    const [allRecipes, setAllRecipes] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/recipes/2")
+            .then(res => res.json())
+            .then(data => {
+                const selected = data.recipes.slice(1, 4);
+                setAllRecipes(selected)
+            })
+            .catch(error => console.error(error))
+    }, [])
+
+    return (
+        <div className='mt-20 w-[85%] mx-auto'>
+            <div className='text-center mb-8'>
+                <h2 className='text-dark hover:text-green-start text-4xl font-bold mb-3 font-playfair'>Our Popular Recipes</h2>
+                <p className='text-gray font-medium'>Explore our chefs for the best food recipes</p>
+            </div>
+            <div className='grid grid-cols-3 gap-6 mt-20'>
+                {
+                    allRecipes.map(recipe => <Recipe
+                        key={recipe.id}
+                        recipe={recipe}
+                    ></Recipe>)
+                }
+            </div>
+            <ToastContainer />
+        </div>
+    );
+};
+
+export default Popular;
