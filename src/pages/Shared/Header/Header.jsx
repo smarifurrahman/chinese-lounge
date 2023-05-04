e034ec6ac0391a283fb6ae5bcc3425638e3becd8
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-04 19:50:57 +0600

    navbar responsive completed

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -10,15 +10,19 @@ const Header = () => {
     return (
         <div className="navbar bg-base-100 w-[85%] mx-auto py-5 flex items-center">
             <div className="navbar-start">
-                {/* <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden mr-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li><a>Item 3</a></li>
+                    <li><ActiveRoute to="/">Home</ActiveRoute></li>
+                    <li><ActiveRoute to="/blogs">Blog</ActiveRoute></li>
+                    {
+                        user ? "" :
+                            <li><ActiveRoute to="/registration">Register</ActiveRoute></li>
+                    }
                     </ul>
-                </div> */}
+                </div>
                 <Link to="/" className="normal-case text-[21px] hover:text-green-start">Chinese Lounge</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
