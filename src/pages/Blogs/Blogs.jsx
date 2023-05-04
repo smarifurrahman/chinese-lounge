46ca28a06c03a5174a0875a8798cdc3edf1dcae6
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-04 22:50:33 +0600

    download pdf button fuctionality implemented

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Blog from './Blog/Blog';
 import { FaFileDownload } from "react-icons/fa";
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -16,6 +18,20 @@ const Blogs = () => {
             .catch(error => console.error(error))
     }, [])
 
+    const downloadPDF = () => {
+        const capture = document.querySelector('#blogs');
+        setLoading(true);
+        html2canvas(capture).then((canvas) => {
+            const imgData = canvas.toDataURL('img/png');
+            const doc = new jsPDF('p', 'mm', 'a4');
+            const componentWidth = doc.internal.pageSize.getWidth();
+            const componentHeight = doc.internal.pageSize.getHeight();
+            doc.addImage(imgData, 'PNG', 0, 0, componentWidth, componentHeight);
+            setLoading(false);
+            doc.save('blogs.pdf');
+        })
+    }
+
     if (loading) {
         return <div className='w-[85%] mx-auto mt-10 text-center'>
             <progress className="progress w-56"></progress>
@@ -23,13 +39,13 @@ const Blogs = () => {
     }
 
     return (
-        <div className='w-[85%] mx-auto'>
+        <div id='blogs' className='w-[85%] mx-auto'>
             <div className='text-center py-14'>
                 <h2 className='text-dark hover:text-green-start text-4xl font-bold mb-3 font-playfair'>Blogs</h2>
             </div>
             <div className='flex justify-end mb-6'>
                 <div className='inline-block'>
-                    <div className='bg-green-start px-4 py-1 text-white rounded-md flex items-center gap-2 hover:bg-green-end cursor-pointer'>
+                    <div onClick={downloadPDF} className='bg-green-start px-4 py-1 text-white rounded-md flex items-center gap-2 hover:bg-green-end cursor-pointer'>
                         <span>Save as pdf</span>
                         <span><FaFileDownload /></span></div>
                 </div>
